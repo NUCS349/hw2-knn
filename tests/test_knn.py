b0eb301177fa1460558a636d7dad85e482d07911
Ethan Manilow <ethanmanilow@gmail.com>
2019-04-18 06:28:49 +0000

    Update test_knn.py

--- tests/test_knn.py
+++ tests/test_knn.py
@@ -3,21 +3,13 @@ from code import KNearestNeighbor, load_json_data
 from code import accuracy
 import os
 
-datasets = [
-    os.path.join('data', x)
-    for x in os.listdir('data')
-    if os.path.splitext(x)[-1] == '.json' 
-]
-
-def xp_dataset_name(key):
-    dataset = [d for d in datasets if key in d]
-    if not dataset:
-        raise ValueError('Dataset ' + key + ' cannot be found')
-    return dataset[0]
-
 
 def test_k_nearest_neighbor():
-    accuracies = {}
+    datasets = [
+        os.path.join('data', x)
+        for x in os.listdir('data')
+        if os.path.splitext(x)[-1] == '.json' 
+    ]
     for data_path in datasets:
         features, targets = load_json_data(data_path)
         model = KNearestNeighbor(1)
