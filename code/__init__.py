bc3086233863aeed231dd38c61a79486691c2c2a
Ethan Manilow <eth@nmanilow.com>
2019-04-19 13:36:57 -0500

    removing pearson

--- code/__init__.py
+++ code/__init__.py
@@ -2,5 +2,5 @@ from .k_nearest_neighbor import KNearestNeighbor
 from .load_json_data import load_json_data
 from .load_movielens import load_movielens_data
 from sklearn.metrics import accuracy_score as accuracy
-from .distances import pearson_distances, cosine_distances, euclidean_distances, manhattan_distances
+from .distances import cosine_distances, euclidean_distances, manhattan_distances
 from .collaborative_filtering import collaborative_filtering
