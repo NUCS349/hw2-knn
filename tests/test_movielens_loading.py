c63306bb034269108a9434ce82685b1d0217988e
bongjun <bongjun@u.northwestern.edu>
2019-10-10 15:18:49 -0500

    adding test conditions for loading data

--- tests/test_movielens_loading.py
+++ tests/test_movielens_loading.py
@@ -6,4 +6,4 @@ def test_load_movielens_data():
     data = load_movielens_data(
         os.path.join('data',  'ml-100k')
     )
-    assert (data.shape == (943, 1682))
+    assert (data.shape == (943, 1682) and np.count_nonzero(data) == 100000)
