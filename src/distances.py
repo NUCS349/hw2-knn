51f61e4142efb7d4e68768f1c6b436fab377b72b
Mike D'Arcy <m.m.darcy@u.northwestern.edu>
2020-10-07 21:15:08 -0500

    Remove cosine distance
    It was removed from other files already but not distances.py, so this commit
    makes everything consistent again.

--- src/distances.py
+++ src/distances.py
@@ -29,17 +29,3 @@ def manhattan_distances(X, Y):
     """
     raise NotImplementedError()
 
-
-def cosine_distances(X, Y):
-    """Compute Cosine distance between the rows of two matrices X (shape MxK) 
-    and Y (shape NxK). The output of this function is a matrix of shape MxN containing
-    the Cosine distance between two rows.
-    
-    Arguments:
-        X {np.ndarray} -- First matrix, containing M examples with K features each.
-        Y {np.ndarray} -- Second matrix, containing N examples with K features each.
-
-    Returns:
-        D {np.ndarray}: MxN matrix with Cosine distances between rows of X and rows of Y.
-    """
-    raise NotImplementedError()
